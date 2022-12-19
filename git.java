0f06037cdc428acfab15c3415c9557fde027d6dc
Lalithambika2 <121012974+Lalithambika2@users.noreply.github.com>
2022-12-19 14:41:07 +0530

    Add files via upload

---
+++ git.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Mul
+{
+    public static void main(String...args)
+    {
+       int n=7;
+       for(i=1;i<=10;i++)
+       {
+            System.out.println(n+"*"+i+"="+n*i);
+       }
+     }
+}
+          
